8c86aad2a6a676aa81c79c4125495c4215d6233a
KOTHECODE <160642762+KOTHECODE@users.noreply.github.com>
2024-06-04 00:13:35 +0000

    Project Complete

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 import { renderToStaticMarkup } from "react-dom/server";
 
 
-//create your first component
-const Home = (props) => {
-	return (
-		<>
+const Counter = (props) => {
+	return <>
 		<div className="container">
 			<div className="boxNum">
 				<i class="far fa-clock"></i>
@@ -22,28 +20,54 @@ const Home = (props) => {
 			<div className="boxNum">{props.digitTwo}</div>
 			<div className="boxNum">{props.digitOne}</div>
 		</div>
+	</>
+}
+
+
+//create your first component
+const Home = (props) => {
+	const [counter, setCounter] = useState(0);
+	useEffect(() => {
+			//component mounting
+		const interval = setInterval(()=>{
+			setCounter(counter => counter + 1);
+		}, 1000)
+	}, [])
+
+	// useEffect(() => {
+	// 	console.log(calculateSeconds(counter, 1));
+	// }, [counter])
+
+	return (
+		<>
+		<Counter
+			digitSix = {calculateSeconds(counter,100000)}
+			digitFive = {calculateSeconds(counter,10000)}
+			digitFour = {calculateSeconds(counter,1000)}
+			digitThree = {calculateSeconds(counter,100)}
+			digitTwo = {calculateSeconds(counter,10)}
+			digitOne = {calculateSeconds(counter,1)}
+		/>
 		</>
 	);
 };
 
-SimpleCounter.propTypes = {
-	digitSix: PropTypes.number,
-	digitFive: PropTypes.number,
-	digitFour: PropTypes.number,
-	digitThree: PropTypes.number,
-	digitTwo: PropTypes.number,
-	digitOne: PropTypes.number,
 
+function calculateSeconds(aCounter, placeValue){
+	return Math.floor(aCounter/placeValue) % 10
 }
-let counter = 0;
-setInterval(function(){
-const one = Math.floor(counter/10)
-const two = Math.floor(counter/100)
-const three = Math.floor(counter/1000)
-const four = Math.floor(counter/10000)
-const five = Math.floor(counter/100000)
-const six = Math.floor(counter/1000000)
-counter ++;
-},1000);
-export default Home;
 
+// return(
+// 	<>
+// 		<simpleCounter
+// 			digitSix = {calculateSeconds(counter,100000)}
+// 			digitFive = {calculateSeconds(counter,10000)}
+// 			digitFour = {calculateSeconds(counter,1000)}
+// 			digitThree = {calculateSeconds(counter,100)}
+// 			digitTwo = {calculateSeconds(counter,10)}
+// 			digitOne = {calculateSeconds(counter,1)}
+// 		/>
+// 	</>
+// )
+
+export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-import PropTypes from  'prop-types';
 
 
 // include your styles into the webpack bundle
@@ -11,4 +10,4 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home digitOne={counter.indexOf()} />, document.querySelector("#app"));
+ReactDOM.render(<Home />, document.querySelector("#app"));
